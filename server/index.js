98c4112645007b5c350e5504a67b9896248bda79
juliecoding <julie.akohler@gmail.com>
2017-04-22 23:45:56 -0600

    Baby got back. End.

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ var twitter_params = {
 }
 
 var watson_params = {
-    text: "Doing a small Python project. Caring about #whitespace is the worst!Currently reading: https://t.co/7v4G5AZpvs Dated but educational! #js #designpatternsIncredible chants. https://t.co/iQiljN2WjE #NowPlayingLoving this playlist of Russian listening exercises! https://t.co/PF4rJ8N6z7Tabby Cat is my favorite \"pointless\" Chrome extension: https://t.co/0Ufw5aLaevThe hell is happening?!?!?!?! https://t.co/a3gWvjL4gW@RusEmbUSA Appalling reports of abuse of gays in Chechnya. Russian govt must take action. https://t.co/yTt1fgvXX3@SenMikeLee and @senorrinhatch Please help pressure Russia to stop Chechen Authorities Arresting and Killing Gay Men https://t.co/HriC0QYsJO@jasoninthehouse Please help pressure Russia to stop Chechen Authorities Arresting and Killing Gay Men https://t.co/yTt1fgvXX3Most are familiar but still a bright and colorful treat. https://t.co/e14feuNhJ5Today's agenda: JS problems, then 8 hours of Node tutorial. May have a mini-app to share by the end of it.Humanizing a crisis ⚡️ “A daughter's freedom weighed against her siblings' lives” by @ReutersWorld https://t.co/SiOaI5OU6cGiant robot arm can build a brick house in 48 hours. To be commercially available late 2017. https://t.co/hx3MRlCi4u via @mashableI realize it's the Daily Mail but informative piece on the history of sarin. https://t.co/Xr2jBbp2CGSo much awesome progress in the last few years! Most advanced self-driving cars you can buy today @Thrillist https://t.co/x3Og2awvNZ@jasoninthehouse I'm salty but that's because my best friend is fighting cancer and would be financially devastated… https://t.co/2GFCiIr93z@jasoninthehouse Not gutting coverage for poor Americans = governing.Bloomberg on UT and the Am Dream https://t.co/MQGpNmLAlP via @BV Some right, some wrong, overall interesting to hear outside POV@KarinnaAnne So pretty!My cousin is the best https://t.co/mzJf0G2ghc@KarinnaAnne you should follow @designprobs. It's hilarious.😭 Made me emotional https://t.co/ALnVhzUI8WYou know you're not in academia anymore when your resume is one page again. #ivorytowerprobsDisturbing https://t.co/2D4LupDuHs@SenMikeLee @senorrinhatch @jasoninthehouse Congrats on undermining Americans' right to privacy. #SJRes34As a long-time Windows user, I found this guide to symbols and shortcuts very helpful: https://t.co/WVcPkj3BQh"
+    text: "" //TWEET TEXT GOES HERE 
 }
 
 app.get('/api/tweets', function(req, res, next) {
@@ -50,20 +50,73 @@ app.get('/api/tweets', function(req, res, next) {
 
 
 
-
-
-
-
 app.get('/api/pi', function(req, res, next) {
     personality_insights.profile(watson_params, function(error, response) {
         if (error) {
             console.log('Error:', error)
         } else {
-            console.log(JSON.stringify(response, null, 2));
+            //var thingToFlatten = response;
             res.status(200).send(response);
         }
     })
 });
+
+function flatten(returned_personality_object) {
+    var data = {};
+    for (var prop in returned_personality_object['tree']['children']) {
+        if (prop.children) {
+            for (var c2 in prop) {
+                if (c2.children) {
+                    for (var c3 in c2['children']) {
+                        if (c3.children) {
+                            for (var c4 in c3['children']) {
+                                if (c4['category'] == 'personality') {
+                                    data[c4['id']] = c4[percentage]
+                                }
+                                if (!'children' in c3) {
+                                    if (c3['category'] == 'personality') {
+                                        data[c3['id']] = c3[percentage];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return data;
+}
+
+// PERSONALITY LOOKS LIKE SO: 
+// data_set = obj
+// tree = obj
+// children = array of objects (personality, needs, values) 
+// (prop) children = array of objects (e.g., Openness_parent)
+// (prop) children = array of objects (e.g., Openness)
+// (prop) children = array of objects (e.g., Adventurousness)
+
+
+// NEEDS LOOKS LIKE SO: 
+// data_set = obj
+// tree = obj
+// children = array of objects (personality, needs, values) 
+// (prop) children = array of objects (e.g., Stability)
+// (prop) children = array of objects (e.g., Challenge and Structure)
+
+
+// VALUES LOOKS LIKE SO: 
+// data_set = obj
+// tree = obj
+// children = array of objects (personality, needs, values)
+// (prop) children = array of objects (e.g., Conservation_parent)
+// (prop) children = array of objects (e.g., Conservation, Self-transcendence)
+
+
+
+flatten(dataToFlatten);
+
+
 
 var port = config.PORT;
 
