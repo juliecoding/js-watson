b28d424a4d698f5131ac9965080f6f576f3d51df
juliecoding <julie.akohler@gmail.com>
2017-10-03 21:08:46 -0600

    Works with staged OR live data

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,45 +1,49 @@
 angular.module('watsonApp').controller('mainController', function($scope, mainService) {
 
-  var allTheLonelyPeople = mainService.allTheLonelyPeople;
+  $scope.arrayOfTraitObjects = [];
 
-  $scope.getTraits = function(name) {
+  let allTheLonelyPeople = mainService.allTheLonelyPeople;
+
+  const sortFunction = (a, b) => {
+    if (a[1] === b[1]) {
+      return 0;
+    } else {
+      return (a[1] < b[1] ? -1 : 1);
+    }
+  }
+
+  $scope.getTraitsStatic = function(name) {
     if (allTheLonelyPeople[name]) {
-      $scope.traitKeys = Object.keys(allTheLonelyPeople[name]);
-      $scope.celebTraits = allTheLonelyPeople[name];
+      $scope.arrayOfTraitObjects = allTheLonelyPeople[name];
     }
   };
 
 
   $scope.getTraits2 = function(profile1, profile2) {
-    mainService.compare(profile1, profile2).then(function(response) {
-      let workableArray = [];
-      const sortStart = (obj) => {
-        for (var prop in obj) {
-          workableArray.push([prop, obj[prop]]);
-        }
-        const sortFunction = (a, b) => {
-          if (a[1] === b[1]) {
-            return 0;
-          } else {
-            return (a[1] < b[1] ? -1 : 1);
+    mainService.compare(profile1, profile2)
+      .then(function(response) {
+        let workableArray = [];
+        const sortStart = (obj) => {
+          for (var prop in obj) {
+            workableArray.push([prop, obj[prop]]);
+          }
+          workableArray = workableArray.sort(sortFunction).splice(0, 5);
+        };
+
+        for (let i = 0; i < workableArray.length; i++) {
+          if (workableArray[i]) {
+            let traitsObj = {};
+            traitsObj.trait = workableArray[i][0]
+            traitsObj.commonality = workableArray[i][1];
+            traitsObj.me = mainService.prof1[workableArray[i][0]];
+            traitsObj.celeb = mainService.prof2[workableArray[i][0]];
+            $scope.arrayOfTraitObjects.push(traitsObj);
           }
         }
-        workableArray = workableArray.sort(sortFunction).splice(0, 5);
-      };
-      $scope.theKingAndI = sortStart(response);
-
-      $scope.arrayOfTraitObjects = [];
-      for (let i = 0; i < workableArray.length; i++) {
-        if (workableArray[i]) {
-          let traitsObj = {};
-          traitsObj.trait = workableArray[i][0]
-          traitsObj.commonality = workableArray[i][1];
-          traitsObj.me = mainService.prof1[workableArray[i][0]];
-          traitsObj.celeb = mainService.prof2[workableArray[i][0]];
-          $scope.arrayOfTraitObjects.push(traitsObj);
-        }
-      }
-    });
+      })
+      .catch(function(error) {
+        console.log(error);
+        $scope.getTraitsStatic(profile2);
+      });
   }
-
 });
--- public/mainService.js
+++ public/mainService.js
@@ -30,42 +30,165 @@ angular.module('watsonApp').service('mainService', function($http) {
 
   //Matched Personality Trait -> Probability of your profile exhibiting given trait -> Probability of Celebrity's profile exhibiting given trait.
   this.allTheLonelyPeople = {
-    kim: {
-      'Achievement striving': [0.332847185565, 0.347230623563, 0.0143834379983],
-      'Vulnerability': [0.92062189458, 0.847154386945, 0.0734675076353],
-      'Trust': [0.426188167294, 0.5011449497, 0.0749567824052],
-      'Activity level': [0.455105766665, 0.541341110874, 0.0862353442095],
-      'Altruism': [0.384503757388, 0.285584235948, 0.0989195214401]
-    },
-    kanye: {
-      'Artistic interests': [0.855514396943, 0.89090915008, 0.0353947531368],
-      'Dutifulness': [0.297568038059, 0.233987447492, 0.0635805905671],
-      'Sympathy': [0.686682548004, 0.755777421461, 0.0690948734566],
-      'Adventurousness': [0.477271717841, 0.392230481025, 0.0850412368163],
-      'Imagination': [0.954898082858, 0.831248403438, 0.12364967942]
-    },
-    lebron: {
-      'Cooperation': [0.307454961435, 0.299933523532, 0.00752143790319],
-      'Emotionality': [0.422932638109, 0.460178369357, 0.0372457312479],
-      'Sympathy': [0.686682548004, 0.625937977503, 0.0607445705011],
-      'Immoderation': [0.355459669631, 0.209639471436, 0.145820198194],
-      'Adventurousness': [0.477271717841, 0.641580907765, 0.164309189924]
-    },
-    snowden: {
-      'Cheerfulness': [0.00212302203007, 0.000440663870174, 0.0016823581599],
-      'Gregariousness': [0.004474788821, 0.00694178845374, 0.00246699963274],
-      'Intellect': [0.98051395313, 0.997802288268, 0.0172883351373],
-      'Altruism': [0.384503757388, 0.408920507714, 0.0244167503264],
-      'Friendliness': [0.0216882806265, 0.0485652688621, 0.0268769882356]
-
-    },
-    trump: {
-      'Intellect': [0.98051395313, 0.96759102067, 0.0129229324597],
-      'Liberalism': [0.99730801874, 0.738571018716, 0.258737000024],
-      'Artistic interests': [0.855514396943, 0.595456205669, 0.260058191274],
-      'Emotionality': [0.422932638109, 0.125347005834, 0.297585632275],
-      'Sympathy': [0.686682548004, 0.989201467409, 0.302518919405]
-    }
+    '@KimKardashian': [
+      {
+        trait: 'Achievement striving',
+        me: 0.332847185565,
+        celeb: 0.347230623563,
+        commonality: 0.0143834379983
+      },
+      {
+        trait: 'Vulnerability',
+        me: 0.92062189458,
+        celeb: 0.847154386945,
+        commonality: 0.0734675076353
+      },
+      {
+        trait: 'Trust',
+        me: 0.426188167294,
+        celeb: 0.5011449497,
+        commonality: 0.0749567824052
+      },
+      {
+        trait: 'Activity level',
+        me: 0.455105766665,
+        celeb: 0.541341110874,
+        commonality: 0.0862353442095
+      },
+      {
+        trait: 'Altruism',
+        me: 0.384503757388,
+        celeb: 0.285584235948,
+        commonality: 0.0989195214401
+      }
+    ],
+    '@kanye': [
+      {
+        trait: 'Artistic interests',
+        me: 0.855514396943,
+        celeb: 0.89090915008,
+        commonality: 0.0353947531368
+      },
+      {
+        trait: 'Dutifulness',
+        me: 0.297568038059,
+        celeb: 0.233987447492,
+        commonality: 0.0635805905671
+      },
+      {
+        trait: 'Sympathy',
+        me: 0.686682548004,
+        celeb: 0.755777421461,
+        commonality: 0.0690948734566
+      },
+      {
+        trait: 'Adventurousness',
+        me: 0.477271717841,
+        celeb: 0.392230481025,
+        commonality: 0.0850412368163
+      },
+      {
+        trait: 'Imagination',
+        me: 0.954898082858,
+        celeb: 0.831248403438,
+        commonality: 0.12364967942
+      }
+    ],
+    '@KingJames': [
+      {
+        trait: 'Cooperation',
+        me: 0.307454961435,
+        celeb: 0.299933523532,
+        commonality: 0.00752143790319
+      },
+      {
+        trait: 'Emotionality',
+        me: 0.422932638109,
+        celeb: 0.460178369357,
+        commonality: 0.0372457312479
+      },
+      {
+        trait: 'Sympathy',
+        me: 0.686682548004,
+        celeb: 0.625937977503,
+        commonality: 0.0607445705011
+      },
+      {
+        trait: 'Immoderation',
+        me: 0.355459669631,
+        celeb: 0.209639471436,
+        commonality: 0.145820198194
+      },
+      {
+        trait: 'Adventurousness',
+        me: 0.477271717841,
+        celeb: 0.641580907765,
+        commonality: 0.164309189924
+      }
+    ],
+    '@Snowden': [
+      {
+        trait: 'Cheerfulness',
+        me: 0.00212302203007,
+        celeb: 0.000440663870174,
+        commonality: 0.0016823581599
+      },
+      {
+        trait: 'Gregariousness',
+        me: 0.004474788821,
+        celeb: 0.00694178845374,
+        commonality: 0.00246699963274
+      },
+      {
+        trait: 'Intellect',
+        me: 0.98051395313,
+        celeb: 0.997802288268,
+        commonality: 0.0172883351373
+      },
+      {
+        trait: 'Altruism',
+        me: 0.384503757388,
+        celeb: 0.408920507714,
+        commonality: 0.0244167503264
+      },
+      {
+        trait: 'Friendliness',
+        me: 0.0216882806265,
+        celeb: 0.0485652688621,
+        commonality: 0.0268769882356
+      }
+    ],
+    '@realDonaldTrump': [
+      {
+        trait: 'Intellect',
+        me: 0.98051395313,
+        celeb: 0.96759102067,
+        commonality: 0.0129229324597
+      },
+      {
+        trait: 'Liberalism',
+        me: 0.99730801874,
+        celeb: 0.738571018716,
+        commonality: 0.258737000024
+      },
+      {
+        trait: 'Artistic interests',
+        me: 0.855514396943,
+        celeb: 0.595456205669,
+        commonality: 0.260058191274
+      },
+      {
+        trait: 'Emotionality',
+        me: 0.422932638109,
+        celeb: 0.125347005834,
+        commonality: 0.297585632275
+      },
+      {
+        trait: 'Sympathy',
+        me: 0.686682548004,
+        celeb: 0.989201467409,
+        commonality: 0.302518919405
+      }
+    ]
   }
-
 });
